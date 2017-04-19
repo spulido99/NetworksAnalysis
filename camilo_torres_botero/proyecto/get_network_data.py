a3670f1ad006bdad820a1ffcd51190c29cd03dd8
Camilo Torres Botero <camilo.torresbotero@gmail.com>
2017-04-19 16:52:33 -0500

    Partial commit

--- camilo_torres_botero/proyecto/get_network_data.py
+++ camilo_torres_botero/proyecto/get_network_data.py
@@ -1,7 +1,7 @@
 from pymongo import MongoClient
 
 client = MongoClient('localhost', 27017)
-db = client.caobanutresa_100000_nouns_emojis_adjectives
+db = client.caobanutresa_nouns_emojis_adjectives
 
 vocab = db.tweets_words.distinct('word')
 
@@ -34,10 +34,10 @@ for user in users:
             word_lower = word.lower()
             edges.add((user['id'],word_lower))
 
-edges_file = open('edges_file_caobanutresa_100000_nouns_emojis_adjectives.txt', 'w')
+edges_file = open('edges_file_total_network_undirected.txt', 'w')
 
-print('source\ttarget', file=edges_file)
+print('source\ttarget\ttype', file=edges_file)
 
 for k, v in sorted(edges, key=lambda x: x[1]):
-    print(k+'\t'+v, file=edges_file)
+    print(k+'\t'+v+'\tUndirected', file=edges_file)
 print(len(edges))
